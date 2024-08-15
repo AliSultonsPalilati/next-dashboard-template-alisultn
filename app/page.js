7045e84ef1e6cc6d2557c074798de0dba194b44a
AliSultonsPalilati <105841102222@student.unismuh.ac.id>
2024-08-15 20:15:24 +0800

    Pesan commit terbaru

--- app/page.js
+++ app/page.js
@@ -7,21 +7,21 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen p-6">
-      <div className="flex items-end h-20 p-4 bg-gradient-to-r from-green-500 to-green-800 rounded-lg shrink-0 md:h-52">
+      <div className="flex items-end h-20 p-4 bg-gradient-to-r from-purple-600 to-black rounded-lg shrink-0 md:h-52">
         { <AcmeLogo /> }
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
           <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
-            <a href="https://nextjs.org/learn/" className="text-green-500">
+            <a href="https://nextjs.org/learn/" className="text-purple-500">
               Next.js Learn Course
             </a>
             , brought to you by devnolife - laboratorium-if-unismuh.
           </p>
           <Link
             href="/login"
-            className="flex items-center self-start gap-5 px-6 py-3 text-sm font-medium text-white transition-colors bg-gradient-to-r from-green-500 to-green-800 rounded-lg hover:bg-blue-400 md:text-base"
+            className="flex items-center self-start gap-5 px-6 py-3 text-sm font-medium text-white transition-colors bg-gradient-to-r from-purple-500 to-black rounded-lg hover:bg-blue-400 md:text-base"
 
           >
             <span>Log in</span>
